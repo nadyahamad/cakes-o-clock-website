f48daca3617c7f665eb16e7436cda0c4c5a5f4ff
Nahamad <nahamad83@gmail.com>
2019-12-10 10:09:05 +0000

    modification of header, addition of products pages and navigation

---
+++ js/script-home.js
@@ -0,0 +1,10 @@
+console.log('it works') /*checked to see js is inserted correctly*/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
