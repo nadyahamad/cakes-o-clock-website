832a90149d92221970c9e0fb5dd59d19537c2884
Nahamad <nahamad83@gmail.com>
2019-12-11 21:34:42 +0000

    update of index, css and js for version 0.2.1

--- js/script-home.js
+++ js/script-home.js
@@ -1,29 +1,41 @@
 console.log('it works') /*checked to see js is inserted correctly*/
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += "responsive";
-  } else {
-    x.className = "topnav";
-  }
+// double carousel - Home Page
+    
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-var acc = document.getElementsByClassName("accordion");
+function showSlides(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}    
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+    }
+    x[slideIndex[no]-1].style.display = "block";  
+}
+
+//Cookies Policy alert
+var close = document.getElementsByClassName("closebtn");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+// Loop through all close buttons
+for (i = 0; i < close.length; i++) {
+  // When someone clicks on a close button
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    // Set the opacity of div to 0 (transparent)
+    div.style.opacity = "0";
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+    // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
 }
+    
